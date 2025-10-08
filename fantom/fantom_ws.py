346760633a5556ba8ad36711298571e2f2305a9a
eduardoruiz1999 <eduardogruiz14@gmail.com>
2025-10-08 11:37:31 -0600

    Create fantom_ws.py

---
+++ fantom/fantom_ws.py
@@ -0,0 +1,19 @@
+# WebSocket Endpoints
+FANTOM_WS_ENDPOINTS = [
+    "wss://wsapi.fantom.network",
+    "wss://fantom-rpc.publicnode.com",
+    "wss://fantom.publicnode.com"
+]
+
+# HTTP Endpoints  
+FANTOM_HTTP_ENDPOINTS = [
+    "https://rpcapi.fantom.network",
+    "https://fantom-rpc.publicnode.com",
+    "https://fantom.publicnode.com"
+]
+
+# Testnet Endpoints
+FANTOM_TESTNET_ENDPOINTS = [
+    "wss://wsapi.testnet.fantom.network",
+    "https://rpc.testnet.fantom.network"
+]
